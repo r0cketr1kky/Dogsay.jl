eec20bed16478374c01eaf8f918a837f593a184b
Rakshit Naidu <rakshit.naidu07@gmail.com>
2019-12-25 13:02:37 +0530

    Update Dogsay.jl

--- src/Dogsay.jl
+++ src/Dogsay.jl
@@ -3,13 +3,13 @@ module Dogsay
 # Source of ASCII dog: https://www.asciiart.eu/animals/dogs
 function dogsay(str)
   l = length(str)
-  println("	     |\_/|   ")                  
-  print("            | @ @             | "); printstyled(str, color=:light_red); println(" |");
-  println("	     |   <>              _  ")
-  println("	     |  _/\------____ ((| |))")
-  println("         |               `--' |")
-  println("     ____|_       ___|   |___.")
-  println(" /_/_____/____/_______|")
+  println("\t\t|\_/|   ")                  
+  println("\t\t| @ @             | \t $str");
+  println("\t\t|   <>              _  ")
+  println("\t\t|  _/\------____ ((| |))")
+  println("\t\t|               `--' |")
+  println("\t\t ____|_       ___|   |___.")
+  println("\t /_/_____/____/_______|")
 end
 
 export dogsay
