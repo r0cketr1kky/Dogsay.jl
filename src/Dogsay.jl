de1fa6c60c434c791855309c5ee0cbe77d9fed2f
Rakshit Naidu <rakshit.naidu07@gmail.com>
2019-12-25 16:07:06 +0530

    Update Dogsay.jl

--- src/Dogsay.jl
+++ src/Dogsay.jl
@@ -3,11 +3,11 @@ module Dogsay
 # Source of ASCII dog: https://www.asciiart.eu/animals/dogs
 function dogsay(str)
   l = length(str)
-  println("   |\\_/|   ")                  
-  println("   | @ @     $str");
-  println("   |   <>              _  ")
-  println("   |  _/\\------____ ((| |))")
-  println("   |               `--| |")
+  println("      |\\_/|   ")                  
+  println("      | @ @        $str");
+  println("      |   <>              _  ")
+  println("      |  _/\\------____ ((| |))")
+  println("      |               `--| |")
   println("  ____|_       ___|   |___.")
   println("/_/_____/____/_______|")
 end
